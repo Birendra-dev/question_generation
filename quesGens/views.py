66633fdb720d774e606b46194fc102dfce5da42c
birendra-dev <077bct025@ioepc.edu.np>
2024-10-24 14:12:30 +0545

    all apps scripts are working

--- quesGens/views.py
+++ quesGens/views.py
@@ -1,9 +1,11 @@
+import torch
 from django.shortcuts import render
 from .forms import InputForm
 from apps.questionGeneration import get_question, question_model, question_tokenizer
 from apps.summarization import summarizer, summary_model, summary_tokenizer
-from apps.keywordExtraction import get_keywords  # Assuming this is the file where the keyword extraction code is
-import torch
+from apps.keywordExtraction import get_keywords
+from apps.distractors import get_distractors, s2v
+
 
 def generate_mcq(request):
     if request.method == 'POST':
@@ -11,7 +13,6 @@ def generate_mcq(request):
         if form.is_valid():
             context = form.cleaned_data['context']
             answer = form.cleaned_data['answer']
-            # method = form.cleaned_data['method']
 
             # Step 1: Summarize the context
             summary_text = summarizer(context, summary_model, summary_tokenizer)
@@ -22,13 +23,21 @@ def generate_mcq(request):
             # Step 3: Extract keywords from original context and summarized text
             keywords = get_keywords(context, summary_text)
 
+            # Step 4: Generate distractors for each keyword
+            distractors_dict = {}
+            for keyword in keywords:
+                distractors = get_distractors(keyword, s2v)
+                distractors_dict[keyword] = distractors
+
+
             # Prepare data to be sent to the result template
             result_data = {
                 'context': context,
                 'summary_text': summary_text,
                 'question': question,
                 'answer': answer,
-                'keywords': keywords
+                'keywords': keywords,
+                'distractors_dict': distractors_dict  # Include distractors in the context
             }
 
             return render(request, 'quesGens/result.html', result_data)
